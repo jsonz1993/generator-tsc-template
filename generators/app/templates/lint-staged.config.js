7b14c7ec75de4f3d1a6c2bb56a63424b4fca6646
jsonz1993 <jsonz@qq.com>
2022-06-16 16:15:06 +0800

    fix: lint-staged path error

--- generators/app/templates/lint-staged.config.js
+++ generators/app/templates/lint-staged.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-
+const path = require('path')
 const fs = require('fs')
 const { ESLint } = require('eslint')
 
@@ -13,20 +13,24 @@ const removeIgnoredFiles = async (files) => {
     )
     const filteredFiles = files.filter((_, i) => !isIgnored[i])
     return filteredFiles.join(' ')
-  } catch(e) {
+  } catch (e) {
     console.error('lint-stage eslint error')
     console.error(e.toString())
     process.exit(1)
   }
 }
 
-const generateTSConfig = stagedFilenames => {
+const generateTSConfig = (stagedFilenames) => {
   try {
-    const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'))
+    const configPath = path.join(__dirname, './tsconfig.json')
+    const tsconfig = JSON.parse(fs.readFileSync(configPath, 'utf8'))
     tsconfig.include = stagedFilenames
-    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig))
+    fs.writeFileSync(
+      path.join(__dirname, './tsconfig.lint.json'),
+      JSON.stringify(tsconfig),
+    )
     return 'tsc --noEmit --project tsconfig.lint.json'
-  } catch(e) {
+  } catch (e) {
     console.error('lint-stage tsc error')
     console.error(e.toString())
     process.exit(1)
@@ -34,12 +38,8 @@ const generateTSConfig = stagedFilenames => {
 }
 
 module.exports = {
-  '*': [
-    'prettier --write --ignore-unknown',
-  ],
-  '*.ts': [
-    generateTSConfig,
-  ],
+  '*': ['prettier --write --ignore-unknown'],
+  '*.ts': [generateTSConfig],
   '*.{js,ts}': [
     async (files) => {
       const filesToLint = await removeIgnoredFiles(files)
